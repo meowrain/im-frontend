b38d3fa42a9929499a988846196d72fbffb7151f
meowrain <meowrain@126.com>
2024-08-27 13:57:05 +0800

    feat: 添加登录页面样式和内容

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,40 @@
 <script setup lang="ts">
+import {reactive} from "vue";
 
+const form = reactive({
+  username: "",
+  password: "",
+})
 </script>
 
 <template>
 <div class="im_login">
+  <div class="banner">
+
+  </div>
+  <div class="login_form">
+    <el-form :model="form">
+        <el-form-item>
+          <el-input v-model="form.username" placeholder="用户名"></el-input>
+        </el-form-item>
+        <el-form-item class="item_password">
+          <el-input v-model="form.password" placeholder="密码"></el-input>
+        </el-form-item>
+      <el-form-item class="item_action">
+        <el-checkbox>记住密码</el-checkbox>
+      </el-form-item>
+      <el-form-item class="item_btn">
+        <el-button style="width: 100%" type="primary">登录</el-button>
+      </el-form-item>
+    </el-form>
+    <div class="other_login">
+      <div class="label">第三方登录</div>
+      <div class="icons">
+        <i class="iconfont icon-QQ"></i>
+      </div>
+
+    </div>
+  </div>
 
 </div>
 </template>
@@ -16,5 +47,46 @@
   border-radius: 5px;
   box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.08);
   overflow: hidden;
+}
+.banner {
+  height: 140px;
+  width: 100%;
+  background-image: url("https://blog.meowrain.cn/api/i/2024/08/23/LVgCv61724406672895269922.webp");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+.login_form {
+  padding: 20px 80px;
+  .item_password, .item_action ,.item_btn{
+    margin-bottom: 6px;
+  }
+  .other_login {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .label {
+      font-size: 14px;
+      color: #555;
+      display: flex;
+      align-items: center;
+      width: 100%;
+      justify-content: space-between;
+      &::before, &::after {
+        width: 35%;
+        height: 1px;
+        background-color: #e3e3e3;
+        content: "";
+        display: inline-flex;
+      }
+    }
+    .icons{
+      margin-top: 5px;
+      i{
+        font-size: 36px;
+        cursor: pointer;
+      }
+    }
+  }
 }
 </style>
